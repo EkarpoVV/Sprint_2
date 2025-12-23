4631e6a56bba60d40d0e55409cade7f53da4cd3b
EKarpov <username@yandex.ru>
2025-12-23 21:00:15 +0400

    Sprint_2

---
+++ task_4.py
@@ -0,0 +1,55 @@
+
+class EmployeeSalary:
+    hourly_payment = 400
+    salary = 0
+
+    def __init__(self,name,hours,rest_days,email):
+        self.name = name
+        self.hours = hours
+        self.rest_days = rest_days
+        self.email = email
+
+    @classmethod
+    def get_hours(cls,name, hours, rest_days, email):
+        if hours == None:
+            hours = (7 - rest_days) * 8
+            return cls(name, hours, rest_days, email)
+
+    @classmethod    
+    def get_email(cls,name, hours, rest_days, email):
+        if email == None:
+            email = f"{name}@email.com"
+            return cls(name, hours, rest_days, email)
+        
+    @classmethod
+    def set_hourly_payment(cls, x):
+        cls.hourly_payment = x
+        
+    def salary(self):   
+        salary = self.hours * EmployeeSalary.hourly_payment
+        return salary
+
+
+
+
+###get_hours
+#employeeSalary = EmployeeSalary.get_hours('Evgen', None ,2 , 'mail@mail.ru')
+#print(employeeSalary.__dict__)
+
+###get_hours
+#employeeSalary = EmployeeSalary.get_hours('Evgen', None ,2 , 'mail@mail.ru')
+#print(employeeSalary.__dict__)
+
+###set_hourly_payment
+#EmployeeSalary.set_hourly_payment(4)
+#print(EmployeeSalary.hourly_payment)
+
+###salary
+#employeeSalary = EmployeeSalary('Evgen', 12 ,2 , 'mail@mail.ru')
+#print(employeeSalary.salary())
+
+
+
+
+
+
